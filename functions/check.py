55645fa6b64f21936aeb7cd4a94074c2ae89fca0
Sinesipho Jacobs <sinesipho.jacobs@gmail.com>
2025-03-05 11:31:29 +0200

    Update check.py

--- functions/check.py
+++ functions/check.py
@@ -71,7 +71,7 @@ def post_github_check(results, report_content):
 def run_robot_tests():
     """Run Robot Framework tests, parse results, and post them to GitHub."""
     print(f"Current Working Directory: {os.getcwd()}")
-    output_file = os.path.abspath('./webapp_tests/robot-test-results/output.xml')
+    output_file = os.path.abspath('./output.xml')
     report_file = os.path.abspath('./combined_report.md')
 
     if not validate_output_file(output_file):
