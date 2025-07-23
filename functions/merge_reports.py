26f1b03252d90f32dd318373314c517a9b05ddb9
Sinesipho Jacobs <sinesipho.jacobs@gmail.com>
2025-07-23 20:26:41 +0200

    Update merge_reports.py

--- functions/merge_reports.py
+++ functions/merge_reports.py
@@ -83,7 +83,6 @@ def merge_reports(results_dirs, output_dir):
         f'rebot '
         f'--starttime "{start_time}" '
         f'--endtime "{end_time}" '
-        f'--name "Web Tests" '
         f'--output {output_base}output.xml '
         f'--log {output_base}log.html '
         f'--report {output_base}report.html '
